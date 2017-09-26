6505310590727544fdbc2d05b703e4b7b3b006dd
Richard McCormack <brick@brick.codes>
2017-09-26 13:22:42 -0400

    Add some audio stuff, bump version

---
+++ src/main/java/codes/brick/common/audio/Util.java
@@ -0,0 +1,40 @@
+package codes.brick.common.audio;
+
+import java.util.concurrent.TimeUnit;
+
+public class Util {
+  public static final float RECENT = TimeUnit.NANOSECONDS.convert(100, TimeUnit.MILLISECONDS);
+
+  private static final int EXP_ADJUSTMENT = 3;
+  private static final float MIN_MULTIPLIER = 0.02f;
+
+  /**
+    An exponential curve (rather than a linear curve)
+    matches our ears much better, so we adjust every multiplier by that before we play them.
+
+    @param volumeMultiplier multiplier to adjust
+    @return Adjusted multiplier
+  */
+  public static float expAdjustMultiplier(float volumeMultiplier) {
+    float multiplier = floatPow(volumeMultiplier, EXP_ADJUSTMENT);
+    // Arbitrary minimum; 0.01 is too quiet to hear but is a possible volume from the slider
+    if (multiplier < MIN_MULTIPLIER && multiplier > 0) {
+      multiplier = MIN_MULTIPLIER;
+    }
+    return multiplier;
+  }
+
+  /**
+    Like pow, but for floats. Does NOT do any sort of overflow handling.
+
+    @param value Base
+    @param times Exponent
+  */
+  public static float floatPow(float value, int times) {
+    float sum = value;
+    for (int i = 1; i < times; i++) {
+      sum *= value;
+    }
+    return sum;
+  }
+}
