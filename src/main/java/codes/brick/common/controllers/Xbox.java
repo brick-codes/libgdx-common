8467ccf80509d8d8290aedacec2d5c92262dca16
Richard McCormack <brick@brick.codes>
2017-09-26 11:15:05 -0400

    Fix style

--- src/main/java/codes/brick/common/controllers/Xbox.java
+++ src/main/java/codes/brick/common/controllers/Xbox.java
@@ -13,6 +13,7 @@
  * limitations under the License.
  * ****************************************************************************
  */
+
 package codes.brick.common.controllers;
 
 import com.badlogic.gdx.controllers.Controller;
